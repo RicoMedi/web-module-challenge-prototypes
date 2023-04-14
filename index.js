f3fb730faf5a8975fa212a524d778d7e34f16698
Madcodes831 <r.medina2119@gmail.com>
2023-04-13 17:53:25 -0700

    mvp solved

--- index.js
+++ index.js
@@ -15,10 +15,22 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+this.name = name
+this.age = age
+this.stomach = [];
 }
-
+Person.prototype.eat = function(edible){
+if (this.stomach.length<10){
+  this.stomach.push(edible);
+}
+}
+Person.prototype.poop = function(){
+  this.stomach =[];
+}
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
 
 /*
   TASK 2
@@ -36,11 +48,26 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, mpg) {
+this.model = model
+this.milesPerGallon = mpg;
+this.tank = 0;
+this.odometer = 0;
 }
-
-
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons;
+}
+Car.prototype.drive = function(distance){
+  const driveableMiles = this.tank * this.odometer
+  if (distance <= driveableMiles){
+    this.odometer = this.odometer + distance
+    this.tank = this.tank - (distance / this.milesPerGallon)
+  }else {
+    this.odometer = this.odometer + driveableMiles;
+    this.tank = 0;
+    return `I ran out of fule at ${this.odometer}miles.`;
+  }
+}
 /*
   TASK 3
     - Write a Baby constructor subclassing Person.
@@ -49,10 +76,14 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+ Person.call(this, name, age);
+ this.favoriteToy = favoriteToy;
 }
-
+Baby.prototype = Object.create(Person.prototype)
+Baby.prototype.play = function(){
+  return `${this.name} is ${this.age} years old, and loves to play with ${this.favoriteToy}`
+}
 
 /* 
   TASK 4
